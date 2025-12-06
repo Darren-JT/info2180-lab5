d7a1d71b63442c440e2972ebc4721585b085849f
Darren-JT <darren10212005@gmail.com>
2025-12-05 21:54:58 -0500

    Excercise 5

--- world.js
+++ world.js
@@ -1,8 +1,37 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-    
+
 var search_b = document.getElementById("lookup");
 var result=document.getElementById("result");
+var city_b = document.getElementById("lookup-cities");
+
+city_b.addEventListener("click", function() {//same logic as normal lookup but i added &lookup=cities to url
+
+        var c = document.getElementById("country").value;
+        var r = new XMLHttpRequest();
+
+        var url = "world.php?country=" + c+ "&lookup=cities";
+
+
+        r.onreadystatechange = function() {
+
+
+
+            if (r.readyState === XMLHttpRequest.DONE) 
+            {
+
+                if (r.status === 200)
+                     {
+
+                    var response = r.responseText;
+                    result.innerHTML = response;} 
+            }};
+
+        
+        r.open('GET', url);
+        r.send();
+
+});
     
     search_b.addEventListener("click", function() {
 
--- world.php
+++ world.php
@@ -5,7 +5,6 @@ $password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
 
 
 
@@ -14,9 +13,63 @@ $c = $_GET['country'];
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$c%'");
 $ans = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+$lookup =  isset($_GET['lookup']) ? $_GET['lookup'] : '';
+
+if ($lookup == 'cities') {
+
+  $stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities JOIN countries ON cities.country_code = countries.code WHERE countries.name LIKE '%$c%'");
+  //it really just a different query to do excersize 5 
+  
+  $ans = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  ?>
+
+<table>
+
+    <thead>
+
+        <tr>
+            <th><b>Name<b></th>
+
+            <th><b>District<b></th>
+            <th><b>Population <b></th>
 
 
-?>
+
+
+
+        </tr>
+
+
+    </thead>
+
+
+    <tbody>
+
+        <?php foreach ($ans as $row): ?>
+        <tr>
+            <td><?= $row['name'];?></td>
+
+            <td><?= $row['district'];?></td>
+
+            <td><?= $row['population'];  ?></td>
+            
+
+        </tr>
+
+
+        <?php endforeach; 
+        ?>
+
+    </tbody>
+
+</table>
+
+
+<?php }
+else
+
+{?>
 
 <table>
 
@@ -27,7 +80,7 @@ $ans = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
             <th><b>Continent<b></th>
             <th><b>Independence  <b></th>
-            
+
             <th><b>Head of state<b></th>
 
 
@@ -58,3 +111,4 @@ $ans = $stmt->fetchAll(PDO::FETCH_ASSOC);
     </tbody>
 
 </table>
+<?php } ?>
